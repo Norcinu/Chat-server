6b01862ea53c0154263f54573cec14994575ec45
Steven Gleed <ledbybyson@gmail.com>
2012-05-21 00:04:40 +0100

    Using mongo to store user info.

--- chat-server.js
+++ chat-server.js
@@ -5,6 +5,27 @@ var clients = [];
 
 net.createServer(function(socket) {
     socket.name = socket.remoteAddress + ":" + socket.remotePort;
-    clients.push(socket);
-    
-}
+    clients.push(socket);   
+});
+
+/* Mongo stuff!! */
+var mongo = require('mongodb');
+var Server = mongo.Server;
+var Db = mongo.Db;
+
+var server = new Server('localhost', 27017, {auto_reconnect:true});
+var db = Db('players', server);
+
+db.open(function(err, db) {
+    if (!err) {
+        console.log("we are connected");
+    }
+
+    db.collection('players', function(err, collection) {
+        collection.find(function(err, cursor) {
+            cursor.each(function(err, item) {
+                if (item != null) console.dir(item);
+            });
+        });
+    });
+});
