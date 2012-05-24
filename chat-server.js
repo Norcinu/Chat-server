bdad56747e156ffe7a1c46f030654de2ed724e83
Steven Gleed <ledbybyson@gmail.com>
2012-05-24 16:03:06 +0100

    Added package.json for details

--- chat-server.js
+++ chat-server.js
@@ -1,3 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\nApp (segchat) is running on Node.js ' + process.version);
+}).listen(process.env['app_port'] || 3000);
+
 var net = require('net');
  
 // list of client sockets
